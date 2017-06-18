ee195ab3416f1f58e4c53f325f65e95d2ef00d61
brunomoulin <brunomau@gmail.com>
2017-06-19 01:34:53 +1000

    committing assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Function that caches a given matrix argument 
+## the super assignment operator <<- makes variables visible to the outer scope of its defining environment
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(inverse) inv <<- inverse
+  getInv <- function() inv
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
+  
+        
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
+## Checks for the cached inverse first and returns it
+## If there's no cache, the matrix is solved, stored and then returned
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  inv <- x$getInv()
+  
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  matrix <- x$get()
+  inv <- solve(matrix, ...)
+  x$setInv(inv)
+  inv
+  
 }
+
